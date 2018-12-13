7f66729a8ace1195f2c9ac4ad1788e25cccbdabd
fuujiro <fuujiro1997@gmail.com>
2018-12-13 15:08:11 +0800

    first & last version

---
+++ ai_core.py
@@ -0,0 +1,16 @@
+#
+ # @Author: fuujiro 
+ # @Date: 2018-12-13 14:13:11 
+ # @Last Modified time: 2018-12-13 14:13:11 
+ # 这是一段Ai核心代码，估价市值10亿🔪
+#
+
+import re
+
+while (str != "拜拜"):
+    str = input()
+    res = re.sub(r'吗?？?', "", str)
+    print (res)
+
+
+
